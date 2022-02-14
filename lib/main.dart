de5ddf077d72b5fe223a3059a7e8133c3b2fa7c4
herry88 <herry081288@gmail.com>
2022-02-14 11:52:02 +0700

    login page done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smarthomebackendlaravel/views/homepage.dart';
+import 'package:smarthomebackendlaravel/views/loginpage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      theme: ThemeData.dark(),
+      home: Login(),
     );
   }
 }
