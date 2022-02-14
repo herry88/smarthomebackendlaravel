b74e541fe12ea1d5f1cc93b583583a0257ca050c
herry88 <herry081288@gmail.com>
2022-02-14 14:50:21 +0700

    simple smarthome

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:smarthomebackendlaravel/views/homepage.dart';
 import 'package:smarthomebackendlaravel/views/lightpage.dart';
-import 'package:smarthomebackendlaravel/views/loginpage.dart';
 
 void main() {
   runApp(const MyApp());
